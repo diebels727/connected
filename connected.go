fe6323855d66dec47bf70a44648f7e0cfb5e2751
diebels727 <diebelsalternative@hotmail.com>
2014-04-21 17:56:46 -0400

    http listening

--- connected.go
+++ connected.go
@@ -1,32 +1,19 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
+    // "encoding/json"
+    "fmt"
     "net/http"
+    "io/ioutil"
 )
 
-func connect(rw http.ResponseWriter, req *http.Request) {
-  decoder := json.NewDecoder(req.Body)
-  log.Println(serving)
-    //
-    // req.ParseForm()
-    // log.Println(req.Form)
-    // //LOG: map[{"test": "that"}:[]]
-    // var t test_struct
-    // for key, _ := range req.Form {
-    //     log.Println(key)
-    //     //LOG: {"test": "that"}
-    //     err := json.Unmarshal([]byte(key), &t)
-    //     if err != nil {
-    //         log.Println(err.Error())
-    //     }
-    // }
-    // log.Println(t.Test)
-    // //LOG: that
+func connect(rw http.ResponseWriter,req *http.Request) {
+  // decoder := json.NewDecoder(req.Body)
+  body, _ := ioutil.ReadAll(req.Body)
+  fmt.Fprintf(rw,"parsed: "+string(body))
 }
 
 func main() {
-    http.HandleFunc("/connect",connect)
-    log.Fatal(http.ListenAndServe(":8080",nil))
+  http.HandleFunc("/connect",connect)
+  http.ListenAndServe(":9091",nil)
 }
---
+++ post.sh
@@ -0,0 +1,1 @@
+curl -v -X POST -H "Content-Type: application/json" -d '{"username":"xyz","password":"xyz"}' http://localhost:9091/connect
