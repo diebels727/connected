7f9fcff99c4ae527151f0c073895cf59f6593938
diebels727 <diebelsalternative@hotmail.com>
2014-04-21 18:05:01 -0400

    trying to unmarshal.

--- connected.go
+++ connected.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-    // "encoding/json"
+    "encoding/json"
     "fmt"
     "net/http"
     "io/ioutil"
 )
 
+type Message struct {
+    p string
+    q string
+}
+
 func connect(rw http.ResponseWriter,req *http.Request) {
   // decoder := json.NewDecoder(req.Body)
+  var m Message
   body, _ := ioutil.ReadAll(req.Body)
-  fmt.Fprintf(rw,"parsed: "+string(body))
+  err := json.Unmarshal(body,&m)
+  if err != nil {
+    fmt.Fprintf(rw,"error")
+  }
+  fmt.Fprintf(rw,"p: %s\nq: %s\n",m.p,m.q)
 }
 
 func main() {
