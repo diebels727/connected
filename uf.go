298de999ca9425e946bc3e3d6bdd7408c95a8ac7
diebels727 <diebelsalternative@hotmail.com>
2014-04-21 22:15:45 -0400

    Add pair.

--- uf.go
+++ uf.go
@@ -4,19 +4,24 @@ import (
   "fmt"
 )
 
+type Pair struct {
+  p int
+  q int
+}
+
 func find(p int,id []int) int {
   return id[p]
 }
 
-func connected(p int,q int,id []int) bool {
-  return id[p] == id[q]
+func connected(pair Pair,id []int) bool {
+  return id[pair.p] == id[pair.q]
 }
 
-func union(p int,q int,id []int) []int {
-  if (connected(p,q,id)) { return id };
-  pid := id[p];
+func union(pair Pair,id []int) []int {
+  if (connected(pair,id)) { return id };
+  pid := id[pair.p];
   for i:=0;i<len(id);i++ {
-      if (id[i] == pid) { id[i] = id[q] };
+      if (id[i] == pid) { id[i] = id[pair.q] };
   }
   return id
 }
@@ -26,14 +31,16 @@ func main() {
   for i:=0;i<10;i++ {
     id = append(id,i)
   }
-  id = union(1,2,id)
-  id = union(3,4,id)
-  id = union(2,3,id)
-  id = union(4,5,id)
-  id = union(6,7,id)
-  id = union(0,7,id)
-  id = union(9,8,id)
-  id = union(7,1,id)
+  var p Pair
+  p = Pair{1,2}
+  id = union(p,id)
+  // id = union(3,4,id)
+  // id = union(2,3,id)
+  // id = union(4,5,id)
+  // id = union(6,7,id)
+  // id = union(0,7,id)
+  // id = union(9,8,id)
+  // id = union(7,1,id)
   fmt.Println(id)
 }
 
