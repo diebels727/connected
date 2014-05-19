dc8884619d800d22f52cf6919850230c7043e192
diebels727 <diebelsalternative@hotmail.com>
2014-05-18 20:22:04 -0400

    go log format

--- main.go
+++ main.go
@@ -6,22 +6,27 @@ import (
   "log"
 )
 
+const (
+  LOG_FORMAT = 3
+)
+
 var id []int
 var ready chan bool
 var pchan chan Pair
 var log_file *os.File
+var logger *log.Logger
 
 func main() {
+  log_file,_ = os.Create("connected.log")
+  logger = log.New(log_file,"",LOG_FORMAT)
+  logger.Printf("Starting up ...")
+
   id = make([]int,0)
   for i:=0;i<10;i++ {
     id = append(id,i)
   }
   ready = make(chan bool,0)
   pchan = make(chan Pair,0)
-
-  log_file,_ = os.Create("connected.log")
-  logger := log.New(log_file,"",0)
-  logger.Printf("Foo!")
 
   go func(){
     for {
